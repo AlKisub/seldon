e981fef3895f0cb67dce1d0243bd592b5b66f6e7
Alex <>
2023-04-02 22:37:30 +0300

    Добавление в админку

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Post, Point
+from .models import Post, Point, Photo
 
 admin.site.register(Post)
 admin.site.register(Point)
+admin.site.register(Photo)
