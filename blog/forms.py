ba229b982b4877068dfbfa15de3a2bcf89ae36a9
Alex <>
2023-04-03 04:16:12 +0300

    Внешний вид форм

--- blog/forms.py
+++ blog/forms.py
@@ -3,12 +3,22 @@ from .models import Post, Point
 
 
 class PostForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(PostForm, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
+
     class Meta:
         model = Post
         fields = ('title', 'text', 'map_href')
 
 
 class PointForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(PointForm, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
+
     class Meta:
         model = Point
         fields = ('post', 'title', 'sequence_number', 'text', 'map_href',)
